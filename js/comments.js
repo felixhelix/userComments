1978ae9fa0ce1c5d1a1fac58cc4e41bbc8e6174d
Stephan Henn <shenn@ub.uni-koeln.de>
2023-12-21 21:09:58 +0100

    changed code to use nested dynamic components

--- js/comments.js
+++ js/comments.js
@@ -3,55 +3,44 @@ const App = Vue.createApp({
     return {
     user: null,
     userComments: [],
-    userCommentsTree: [],
     dataFetched: false,
     commentText: '',
-    replyText: '',
-    buttonId: 'button_null',
     apiKey: '',
-    csrfToken: ''
+    csrfToken: '',
+    foreignCommentId: null,
+    commentAction: 'formButton' //'commentForm'
     }
   },
   mounted() {     
     // Read some props from the root element
-    this.originalParentNode = this.$el.parentNode;
-    this.apiKey = this.originalParentNode.dataset.apikey;
-    this.csrfToken = this.originalParentNode.dataset.csrftoken;   
-    this.user = this.originalParentNode.dataset.user;   
+    this.apiKey = this.$el.parentNode.dataset.apikey;
+    this.csrfToken = this.$el.parentNode.dataset.csrftoken;   
+    this.user = this.$el.parentNode.dataset.user;   
     // Fetch data from the API when the component is mounted
     this.fetchData();
   },
   methods: {
-    moveForm(userCommentid) {
+    moveForm2(userCommentId, event) {
+      // replace the button with the form component
+      commentAction = 'commentForm';
+    },
+    moveForm(userCommentId, event) {
       // remove form from current parent node
       formNode = document.getElementById("userCommentForm");
       oldParentNode = formNode.parentNode;
       oldParentNode.removeChild(formNode);
       // get the new parent node
-      newParentNode = document.getElementById(this.createFormId(userCommentid));
+      newParentNode = event.target.parentNode;
       newParentNode.appendChild(formNode);
       // set the foreignCommentId to the commentId
-      formNode.querySelector('#foreignCommentId').value = userCommentid;
+      // formNode.querySelector('#foreignCommentId').value = userCommentId;
+      this.foreignCommentId = userCommentId
       // hide the button in the new container
-      button = document.getElementById(this.createCommentId(userCommentid));
-      button.style.display = "none";
+      event.target.style.display = "none";
       // show the button in the old container
       button = oldParentNode.getElementsByTagName('button')[0];
       button.style.display = "block";
-    },   
-    createFormId(userCommentId) {
-      return "comment_" + userCommentId;
-    },
-    createCommentId(userCommentId) {
-      return "comment_" + userCommentId;
-    },      
-    createButtonId(userCommentId) {
-      return "button_" + userCommentId;
-    },    
-    fetchFormData() {
-      // this.submissionId = document.getElementById('submissionId').value;
-      this.foreignCommentId = document.getElementById('foreignCommentId').value;      
-    },
+    },  
     fetchData() {
       this.location = window.location.href;
       this.submissionId = this.location.split("/")[this.location.split("/").length -1];
@@ -64,8 +53,7 @@ const App = Vue.createApp({
         .then(response => response.json())
         .then(data => {
           // Set the fetched data to the component state
-          this.userComments = data;
-          this.userCommentsTree = this.buildTree(data).children;
+          this.userComments = this.buildTree(data).children;
           this.dataFetched = true;
           // Clear the input fields
           this.commentText = '';
@@ -74,9 +62,7 @@ const App = Vue.createApp({
           console.error('Error fetching data:', error);
         });
     },
-    postData() {
-      // Fetch CSRF token and submission data
-      this.fetchFormData();
+    postData(parentComponent, submitEvent) {
       // Make a POST request to the API
       fetch('http://localhost/ops3/index.php/socios/api/v1/userComments/submitComment', {
         method: 'POST',
@@ -85,9 +71,9 @@ const App = Vue.createApp({
           'X-Csrf-Token': this.csrfToken,          
         },
         body: JSON.stringify({
-          commentText: this.commentText,
+          commentText: submitEvent.target.commentText.value,
           submissionId: this.submissionId,
-          foreignCommentId: this.foreignCommentId,
+          foreignCommentId: submitEvent.target.dataset.usercommentid,
           completed: false
         }),
       })
@@ -98,8 +84,8 @@ const App = Vue.createApp({
 
           // Fetch data again to update the displayed list
           this.fetchData();
-          // Re-position the comment field, return a success message, put the focus on the new comment or reply
-          this.moveForm(null); 
+          // close the comment field
+          parentComponent.toggleComment();
         })
         .catch(error => {
           console.error('Error posting data:', error);
@@ -133,24 +119,19 @@ const App = Vue.createApp({
             }
         };
         return {id: item.id, foreignCommentId: item.foreignCommentId, userName: item.userName, commentDate: item.commentDate, commentText: item.commentText, flaggedDate: item.flaggedDate, visible: item.visible, children: childnodes};
-    }
+    }, 
   }
 });
 
 App.component('userCommentsBlock', {
   props: ['userComments'],
+  data() {
+    return {
+      commentAction: 'formButton'
+    }
+  },
   methods: {
-    createFormId(userCommentId) {
-      return "commentForm_" + userCommentId;
-    },
-    createCommentId(userCommentId) {
-      return "comment_" + userCommentId;
-    },  
-    createFlagId(userCommentId) {
-      return "flag_" + userCommentId;
-    },  
     flagComment(userCommentId) {
-      alert("flag comment " + userCommentId);
       // Make a POST request to the API
       fetch('http://localhost/ops3/index.php/socios/api/v1/userComments/flagComment', {
         method: 'POST',
@@ -177,27 +158,10 @@ App.component('userCommentsBlock', {
         .catch(error => {
           console.error('Error posting data:', error);
         });      
-    },
-    moveFormRoot(userCommentid) { 
-      return this.$root.moveForm(userCommentid);
-    },
-    moveForm(userCommentid) {
-      // remove form from current parent node
-      formNode = document.getElementById("userCommentForm");
-      oldParentNode = formNode.parentNode;
-      oldParentNode.removeChild(formNode);
-      // get the new parent node
-      newParentNode = document.getElementById(this.createFormId(userCommentid));
-      newParentNode.appendChild(formNode);
-      // set the foreignCommentId to the commentId
-      formNode.querySelector('#foreignCommentId').value = userCommentid;
-      // hide the button in the new container
-      button = document.getElementById(this.createCommentId(userCommentid));
-      button.style.display = "none";
-      // show the button in the old container
-      button = oldParentNode.getElementsByTagName('button')[0];
-      button.style.display = "block";
-    },    
+    }, 
+    toggleComment() {
+      this.commentAction = (this.commentAction == 'formButton' ? 'commentForm' : 'formButton');
+    }
   },
   template: `
   <ul class="userComments" v-if="userComments && userComments.length">
@@ -205,11 +169,9 @@ App.component('userCommentsBlock', {
       <div class="userComment">
         {{ userComment.commentText }}
         <span class="commentMeta">{{ userComment.userName }} {{ userComment.commentDate }}</span>
-        <button v-if="$root.user && userComment.flaggedDate == null" :id=createFlagId(userComment.id) @click="flagComment(userComment.id)">flag</button>
+        <button v-if="$root.user && userComment.flaggedDate == null" @click="flagComment(userComment.id)">flag</button>
         <div v-if="userComment.flaggedDate != null" style="background-color: red">{{ userComment.flaggedDate}}</div>
-        <div v-if="$root.user" :id=createFormId(userComment.id) :data-commentID=userComment.id>
-            <button :id=createCommentId(userComment.id) @click="moveForm(userComment.id)">reply</button>
-        </div>
+        <form-container :userCommentId=userComment.id></form-container>
       </div>
       <div style="margin-left: 1rem" v-if="userComment.children && userComment.children.length">
         <user-comments-block :user-comments="userComment.children"></user-comments-block>
@@ -218,4 +180,42 @@ App.component('userCommentsBlock', {
 </ul>`
 });
 
+App.component('formContainer', {
+  props: ['userCommentId'],
+  data() {
+    return {
+      commentAction: 'formButton'
+    }
+  },  
+  methods: {
+    toggleComment() {
+      this.commentAction = (this.commentAction == 'formButton' ? 'commentForm' : 'formButton');
+    }
+  },
+  template: `
+    <div v-if="$root.user" :data-commentID=userCommentId>
+      <component :is="commentAction" :userCommentId=userCommentId></component>
+      <!-- button @click="$root.moveForm2(userComment.id, $event)">reply</button -->
+    </div>`
+  });
+  
+App.component('commentForm', {
+  props: ['userCommentId'],
+  template: `
+    <form id="userCommentForm" @submit.prevent="$root.postData($parent, $event)" :data-userCommentId="userCommentId">
+      <label for="commentText">Your comment:</label>
+      <textarea type="text" id="commentText" v-model="$root.commentText" required></textarea>
+      <input id="userCommentId" type="hidden" v-model="userCommentId">
+      <button type="submit">Submit</button>
+      <button @click="$parent.toggleComment()">close</button>
+    </form>`
+});
+
+App.component('formButton', {
+  props: ['userCommentId'],
+  template: `
+    <button @click="$parent.toggleComment()">comment</button>
+    `
+  });
+  
 App.mount('#commentsApp')
--- templates/commentBlock.tpl
+++ templates/commentBlock.tpl
@@ -1,26 +1,16 @@
 <div class="item">
     <h3>Comments</h3>
     <div id="commentsApp" data-user="{$user->getId()}" data-apiKey="{$apiKey}" data-submissionId="{$submissionId}" data-csrfToken="{$csrfToken}">
-        <user-comments-block :user-comments="userCommentsTree"></user-comments-block>
+        <user-comments-block :user-comments="userComments"></user-comments-block>
         <div>
         <h4>Submit a comment</h4>
         {if $user}
         You are logged in as {$user->getFullName()}
-        <div id="comment_null" data-commentID="null">
-            <button style="display:none" @click="moveForm('null')" :id=createButtonId('null')>comment</button>
-            <form id="userCommentForm" @submit.prevent="postData">
-                <input type="hidden" id="csfrToken" value="{$csrfToken}">
-                <input type="hidden" id="foreignCommentId" value="NULL">
-                <label for="commentText">Your comment:</label>
-                <textarea type="text" id="commentText" v-model="commentText" required></textarea>
-                <button type="submit">Submit</button>
-            </form>
-        </div>
+        <form-container :userCommentId=null></form-container>
         {else}
         You have to be logged in to post a comment.
         {/if}
         </div>
     </div>
 </div>
-
 <!--- e-mail@mieterverein-koeln.de --->
